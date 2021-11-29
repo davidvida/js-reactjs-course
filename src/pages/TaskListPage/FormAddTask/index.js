96619271153ba18302828bb01ede821468bc41b8
Sandy Garcia <sandy.garcia.jala@gmail.com>
2021-11-29 14:37:45 -0400

    Task 2: Use Dialog to create new To Do element

--- src/pages/TaskListPage/FormAddTask/index.js
+++ src/pages/TaskListPage/FormAddTask/index.js
@@ -1,12 +1,16 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const FormAddTask = ({ onSubmitCallback }) => {
   const [taskName, setTaskName] = useState("");
+  const [open, setOpen] = React.useState(false);
   const inputRef = useRef();
-
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
 
   const onChangeName = (event) => {
     setTaskName(event.target.value);
@@ -17,28 +21,47 @@ const FormAddTask = ({ onSubmitCallback }) => {
     onSubmitCallback({
       name: taskName
     });
+    setOpen(false);
     setTaskName("");
+  };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
-    <form onSubmit={onSubmitListener}>
-      <div>
-        <label htmlFor="taskName">Task Name</label>
-        <input
-          type="text"
+  <div>
+    <Button variant="outlined" onClick={handleClickOpen}>
+      Open form dialog
+    </Button>
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Task</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          To add a task, please enter your new task here.
+        </DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
           id="taskName"
-          name="name"
-          autoComplete="off"
+          name="taskName"
+          label="New Task"
+          type="text"
+          fullWidth
+          variant="standard"
           ref={inputRef}
-          value={taskName}
-          onChange={onChangeName} />
-      </div>
-      <div>
-        <button type="submit" id="submitForm">
-          Add Task
-        </button>
-      </div>
-    </form>
+          onChange={onChangeName}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={onSubmitListener}>Add Task</Button>
+      </DialogActions>
+    </Dialog>
+  </div>
   );
 };
 
