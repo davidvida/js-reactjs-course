8f9daf3c038966fc31684e5058907f6188a6b3ef
Sandy Garcia <sandy.garcia.jala@gmail.com>
2021-11-29 14:44:51 -0400

    Task 3: Add useNavigate to navigate in To Do pages

--- src/components/ListItemLink/index.jsx
+++ src/components/ListItemLink/index.jsx
@@ -1,12 +1,16 @@
 // component link to route lement here
 // use useNavigation for redirect
+// Help in: https://reactrouter.com/docs/en/v6/api#usenavigate
 import React from "react";
 import { ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 
 const ListItemLink = ({ to, label, icon }) => {
+  let navigate = useNavigate();
 
-  const handleClick = () => {
-    // logic to redirect
+  const handleClick = (event) => {
+    event.preventDefault();
+    navigate(to);
   }
 
   return (
