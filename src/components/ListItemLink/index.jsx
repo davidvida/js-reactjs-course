5333badc5fad066cff67558f9488a94c840308dd
miguelmontalvo <angelmiguel.montalvo@gmail.com>
2021-11-28 23:57:36 -0400

    Task-3 : Added menu navigation functionality.

--- src/components/ListItemLink/index.jsx
+++ src/components/ListItemLink/index.jsx
@@ -2,11 +2,12 @@
 // use useNavigation for redirect
 import React from "react";
 import { ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 
 const ListItemLink = ({ to, label, icon }) => {
-
-  const handleClick = () => {
-    // logic to redirect
+  let navigate = useNavigate();
+  const handleClick = (e) => {
+    navigate(to);
   }
 
   return (
