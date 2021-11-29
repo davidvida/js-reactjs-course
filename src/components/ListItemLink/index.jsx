1c19d3ce493d72b851f13b07884b9782220cf3c4
Efrain Espadero C <espaderoefrain@gmail.com>
2021-11-29 12:02:54 -0400

    Challenge, Make a functional navigation bar using navigate

--- src/components/ListItemLink/index.jsx
+++ src/components/ListItemLink/index.jsx
@@ -2,11 +2,13 @@
 // use useNavigation for redirect
 import React from "react";
 import { ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import { useNavigate } from 'react-router';
 
 const ListItemLink = ({ to, label, icon }) => {
-
+  const navigate = useNavigate();
   const handleClick = () => {
     // logic to redirect
+    navigate(to);
   }
 
   return (
