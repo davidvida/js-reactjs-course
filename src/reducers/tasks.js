8eaf79c4ef69e729cc8c65d01be29e2b88eec7d9
Reinaldo Aguilar <reinaldo.aguilar@communitybrands.com>
2021-12-01 11:25:30 -0400

    Task-redux

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,8 +1,17 @@
 // reducer for UI
 import initialState from './initialState';
+import { SET_LIST } from '../actions/tasks';
 
 const tasksReducer = (state=initialState.tasks, action) => {
-  return state;
+  switch(action.type) {
+    case SET_LIST:
+      return {
+        ...state,
+        data: action.data
+      };      
+    default:
+      return state;
+    }
 };
 
 export default tasksReducer;
