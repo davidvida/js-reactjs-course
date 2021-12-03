8433702979ddafd4ff0336df1917638aad8fe933
miguel montalvo <angelmiguel.montalvo@gmail.com>
2021-12-03 13:55:18 -0400

    Task_4 : Added action to set task :(

--- src/middlewares/tasks.js
+++ src/middlewares/tasks.js
@@ -1,8 +1,9 @@
 import { apiStart, API_FAILURE, API_SUCCESS } from "../actions/api";
-import { FETCH_TASKS, setTasks } from "../actions/tasks";
+import { FETCH_TASKS, POST_TASK, setTasks } from "../actions/tasks";
 import { setLoader, setNotification } from "../actions/ui";
 
 const TASKS_API_GET = "http://localhost:3000/data/tasks.json";
+const TASKS_API_POST = "http://localhost:3000/data/";
 
 export const tasksMiddleware = () => (next) => (action) => {
   next(action);
@@ -18,5 +19,10 @@ export const tasksMiddleware = () => (next) => (action) => {
     case API_FAILURE:
       next(setNotification({error: action.payload}));
       next(setLoader(false));
+      break;
+    case POST_TASK:
+      next(apiStart({body: action.payload, method: 'POST', url: TASKS_API_POST}));
+      next(setLoader(false));
+      break;
   }
 };
