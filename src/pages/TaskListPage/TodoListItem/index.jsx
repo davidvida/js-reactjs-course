9964e91cf987be1133fed19288ab60819a594f00
miguel montalvo <angelmiguel.montalvo@gmail.com>
2021-12-18 16:55:17 -0400

    Final_Task : Added missinga task data.

--- src/pages/TaskListPage/TodoListItem/index.jsx
+++ src/pages/TaskListPage/TodoListItem/index.jsx
@@ -1,42 +1,12 @@
 import React from "react";
+import { format } from 'date-fns';
 import { IconButton, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import PlayIcon from '@mui/icons-material/PlayCircleOutlined';
 import StopRounded from '@mui/icons-material/StopRounded';
 import CompletedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
 import PendingIcon from '@mui/icons-material/PendingOutlined';
-/* styles import */
-import { withStyles } from "@mui/styles";
-import styles from './styles';
-
-/*
-* class based component
-*/
-// class TodoListItem extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-//     const { name, completed, classes } = this.props;
-//     return (
-//       <ListItem
-//         disablePadding
-//         secondaryAction={
-//           <IconButton edge="end" aria-label="play">
-//             <PlayIcon />
-//           </IconButton>
-//         }
-//       >
-//         <ListItemButton>
-//           <ListItemIcon>
-//             { completed ? <CompletedIcon className={classes.icon} /> : <PendingIcon /> }
-//           </ListItemIcon>
-//           <ListItemText primary={name} />
-//         </ListItemButton>
-//       </ListItem>
-//     )
-//   }
-// }
+import Chip from '@mui/material/Chip';
+import Paper from '@mui/material/Paper';
 
 /*
 * function component
@@ -50,6 +20,9 @@ const TodoListItem = ({ name, completed, item, enabled, updateTask }) => {
     event.preventDefault();
     updateTask(item, !completed);
   };
+  const startDate = item.startDate ? new Date(item.startDate) : null;
+  const endDate = item.endDate ? new Date(item.endDate) : null;
+  const labels = item.labels;
   return (
     <ListItem
       disablePadding
@@ -64,10 +37,24 @@ const TodoListItem = ({ name, completed, item, enabled, updateTask }) => {
           { completed ? <CompletedIcon className={classes.icon} /> : <PendingIcon /> }
         </ListItemIcon>
         <ListItemText primary={name} />
+        { startDate ? <ListItemText primary="Start date" secondary={format(startDate, "MMMM d, yyyy H:mma")}/> : <></> }
+        { endDate ? <ListItemText primary="End date" secondary={format(endDate, "MMMM d, yyyy H:mma")}/> : <></> }
+        { labels ? <Paper sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    flexWrap: 'wrap',
+                    listStyle: 'none',
+                    p: 0.5,
+                    m: 0
+                  }}>
+          { labels.map((label, index) => {
+            return (
+              <Chip label={label} />
+            );
+        })} </Paper> : <></> }
       </ListItemButton>
     </ListItem>
   )
 };
 
-// export default withStyles(styles)(TodoListItem);
 export default TodoListItem;
